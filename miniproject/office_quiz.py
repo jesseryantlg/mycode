427d7cb0fd5acb4487cd84fcf3a116eb99c2a8c0
jesseryantlg <jesse.ryan@tlgcohort.com>
2023-01-05 00:42:56 +0000

    miniproject dictionary - office quiz

---
+++ miniproject/office_quiz.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+
+{"question": "Pick a type of paper:",
+        "card stock": "Michael",
+        "printer paper": "Jim ",
+        "newspaper": "Stanley ", 
+        "wrapping paper": "Angela",
+        "glossy paper": "Pam",
+        "papyrus": "Andy ", 
+        "sandpaper": "Dwight",
+        "paper bag": "Kevin",
+        "rolling paper": "Creed",
+        }
+
+{"question": "Pick a home:",
+        "lighthouse":"Creed",
+        "barn":"Dwight",
+        "frat house": "Andy",
+        "private jet": "Michael",
+        "city apartment": "Jim",
+        "brownstone": "Stanley ",
+        "duplex": "Kevin ",
+        "country mansion": "Angela",
+        "quirky cottage":"Pam "}
+
+{"question": "Pick a party theme:",
+        "Mardi Gras":"Michael",
+        "pirates":"Creed",
+        "tea":"Angela",
+        "football":"Jim",
+        "casino":"Stanley",
+        "medieval":"Dwight",
+        "burlesque":"Kevin",
+        "artistic":"Pam ",
+        "crab boil":"Andy"
+        ]
+
+{"question": "Pick a career:",
+        "graphic designer": "Pam",
+        "professor": "Stanley",
+        "writer": "Angela",
+        "survivalist": "Dwight",
+        "bartender": "Kevin",
+        "actor": "Andy",
+        "bitcoin developer": "Creed",
+        "CEO": "Michael",
+        "sports agent": "Jim"
+        }
+
+{"question": "How will you probably die?"
+        "alcohol poisoning":"Michael",
+        "sleeping":"Pam ",
+        "heart attack":"Kevin", 
+        "autoerotic asphyxiation": "Creed ",
+        "NOT POSSIBLE": "Dwight",
+        "lost at sea": "Andy ",
+        "car accident": "Jim ",
+        "sugar overdose": "Stanley",
+        "eaten by cats": "Angela ",
+        }
+{"question": "Pick one thing for your freezer:",
+        "steak": "Andy",
+        "ice cream": "Jim",
+        "ice": "Angela ",
+        "cookie dough": "Kevin",
+        "money": "Creed",
+        "bananas": "Pam",
+        "microwave jalapeno poppers": "Stanley",
+        "venison": "Dwight",
+        "frozen dinner": "Michael",
+        }
+
+{"question": "How did you get in trouble in high school?",
+        "not participating":"Pam",
+        "talking":"Andy",
+        "pulling pranks":"Jim",
+        "eating in class":"Stanley",
+        "absence":"Creed ",
+        "bullying":"Michael",
+        "skipping gym": "Kevin",
+        "arguing with teachers": "Dwight",
+        "didn't happen": "Angela"
+        }
