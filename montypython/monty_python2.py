44e3d6e7123e202e8bf2723ce5007a80ec64d174
jesseryantlg <jesse.ryan@tlgcohort.com>
2023-01-05 22:05:31 +0000

    Error handling examples

--- montypython/monty_python2.py
+++ montypython/monty_python2.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python3
+"""While Loops Lab | Jesse Ryan """
 
-round = 0
+round = 0  #integer starts at zero
 
 while True:
     round = round + 1
     print('Finish the movie title, "Monty Python\'s The Life of _____"')
     answer = input("Your guess --> ")
     if answer == 'Brian':
-        print('Correct')
+        print('Correct!')
 
         break
+    elif round == 3:
+        print('Sorry, the answer was Brian.')
+        break
+    else:
+        print('Sorry. Try again!')
 
 
