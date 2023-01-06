004a12c2918eb2bca19094e1ead81f2d4b1578ad
jesseryantlg <jesse.ryan@tlgcohort.com>
2023-01-06 17:10:14 +0000

    finished office quiz. need to debug wrong inputs on q2-q7

--- miniproject/office_quiz.py
+++ miniproject/office_quiz.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 """A quiz to understand yourself in the context of 'The Office' | Jesse Ryan"""
 
+from collections import Counter
 
+def most_frequent(List):
+    occurence_count = Counter(List)
+    return occurence_count.most_common(1)[0][0]
 
 
 #Prompt user with a welcome
@@ -324,111 +328,10 @@ userprofile.append(q7[q7_response])
 
 responses = responses + 1
 
+#print(responses)
+#print(userprofile)
+
+#print(most_frequent(userprofile))
+print("Thank you for participating in this research study. \n")
+print("Your best friend would be: \n  ")
 print(most_frequent(userprofile))
-print(responses)
-print(userprofile)
-
-
-#remove the comment to test formatting
-#print(q1_response)
-
-#q2= {"question": "Pick a home:",
- #       "lighthouse":"Creed",
-  #      "barn":"Dwight",
-#        "frat house": "Andy",
- #       "private jet": "Michael",
-  #      "city apartment": "Jim",
-   #     "brownstone": "Stanley ",
-    #    "duplex": "Kevin ",
-     #   "country mansion": "Angela",
-      #  "quirky cottage":"Pam "}
-
-
-
-
-
-
-#check the response against the available options
-#if q1_response in q1_list:
-    
-
-
-
-
-
-
-
-
-
-
-#convert
-
-#q2= {"question": "Pick a home:",
-       # "lighthouse":"Creed",
-       # "barn":"Dwight",
-       # "frat house": "Andy",
-       # "private jet": "Michael",
-       # "city apartment": "Jim",
-       # "brownstone": "Stanley ",
-       # "duplex": "Kevin ",
-       # "country mansion": "Angela",
-       # "quirky cottage":"Pam "}
-
-# q3= {"question": "Pick a party theme:",
-       # "Mardi Gras":"Michael",
-       # "pirates":"Creed",
-       # "tea":"Angela",
-       # "football":"Jim",
-      #  "casino":"Stanley",
-       # "medieval":"Dwight",
-       # "burlesque":"Kevin",
-       # "artistic":"Pam ",
-       # "crab boil":"Andy"
-       # ]
-
-# q4= {"question": "Pick a career:",
- #       "graphic designer": "Pam",
-  #      "professor": "Stanley",
-  #      "writer": "Angela",
-  #      "survivalist": "Dwight",
-  #      "bartender": "Kevin",
-  #      "actor": "Andy",
-  #      "bitcoin developer": "Creed",
-  #      "CEO": "Michael",
-  #      "sports agent": "Jim"
-  #      }
-
-# q5= {"question": "How will you probably die?"
- #       "alcohol poisoning":"Michael",
-  #      "sleeping":"Pam ",
-   #     "heart attack":"Kevin", 
-    #    "autoerotic asphyxiation": "Creed ",
-     #   "NOT POSSIBLE": "Dwight",
-      #  "lost at sea": "Andy ",
-       # "car accident": "Jim ",
-       # "sugar overdose": "Stanley",
-       # "eaten by cats": "Angela ",
-       # }
-# q6= {"question": "Pick one thing for your freezer:",
- #       "steak": "Andy",
-  #      "ice cream": "Jim",
-   #     "ice": "Angela ",
-    #    "cookie dough": "Kevin",
-     #   "money": "Creed",
-      #  "bananas": "Pam",
-       # "microwave jalapeno poppers": "Stanley",
-       # "venison": "Dwight",
-       # "frozen dinner": "Michael",
-       # }
-
-# q7= {"question": "How did you get in trouble in high school?",
- #       "not participating":"Pam",
-  #      "talking":"Andy",
-   #     "pulling pranks":"Jim",
-    #    "eating in class":"Stanley",
-     #   "absence":"Creed ",
-      #  "bullying":"Michael",
-       # "skipping gym": "Kevin",
-       # "arguing with teachers": "Dwight",
-       # "didn't happen": "Angela"
-       # }
