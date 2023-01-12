647609181af07e1048dedbc25129e6b1d9d091d7
jesseryantlg <jesse.ryan@tlgcohort.com>
2023-01-12 00:09:27 +0000

    Final Project updates

--- finalproject/finalproject.py
+++ finalproject/finalproject.py
@@ -4,13 +4,19 @@
 
 
 from card_deck import *
-
-#Deck()
-#Deal 5 cards from the deck to three different players
-d = Deck()
+from collections import Counter
+import random
+deck = ["A♦", "A♣", "A♥", "A♠", "2♦", "2♣", "2♥", "2♠", "3♦", "3♣", "3♥", "3♠", "4♦", "4♣", "4♥", "4♠", "5♦", "5♣", "5♥", "5♠", "6♦", "6♣", "6♥", "6♠", "7♦", "7♣", "7♥", "7♠", "8♦", "8♣", "8♥", "8♠", "9♦", "9♣", "9♥", "9♠", "10♦", "10♣", "10♥", "10♠", "J♦", "J♣", "J♥", "J♠", "Q♦", "Q♣", "Q♥", "Q♠", "K♦", "K♣", "K♥", "K♠"]
+pond = []
+p1hand = []
+p2hand = []
+p1matches = []
+p1score = 0
+p2matches = []
+p2score = 0
 
 #view the contents of the deck before dealing
-print("The deck contains: ", d)
+print("The deck contains: ", deck)
 
 #Uncomment the lines below to incorporate user permissions. Leaving commented to work on the game mechanics
 #ready = ["yes", "y"]
@@ -18,55 +24,75 @@ print("The deck contains: ", d)
 
 print("\n")
 
-
-
-print("Dealing...")
-
-
 #else:
  #   print("Please try again. Enter 'y' or 'yes'")
 #  break
+
 #Shuffle the deck
-
 print("Shuffling the deck...")
+print("\n")
+random.shuffle(deck)
 
-d.shuffle()
-print("The deck contains: ", d)
+#d.shuffle()
 
-#Deal 5 cards to two players. 
-#Define the pile
+#Visualize the shuffled deck
+print("The deck contains: ", deck)
+print("\n")
 
-hands = d.deal(2, 5)
+#Deal 5 cards to 2 players
+print("Dealing...")
+print("\n")
 
+for x in range(5):
+    deal = random.choice(deck)
+    p1hand.append(deal)
+    deck.remove(deal)
+
+for x in range(5):
+    deal = random.choice(deck)
+    p2hand.append(deal)
+    deck.remove(deal)
+
+
+#old code - uses card_deck package -hands = d.deal(2, 5)
+
+#create objects for each player's hand,player matches, and the pond
+#p1hand = hands[0]
+#p2hand = hands[1]
+#p1matches = []
+#p1score = 0
+#p2matches = []
+#p2score = 0
+#pond = d
 
 #validate that cards were transferred from the deck to the pile
-print("The players are holding: ", hands)
+print("FOR TESTING PURPOSES - COMMENT OUT - The players are holding: ","\n","Player 1: ", p1hand, "\nPlayer 2: ", p2hand, "\n")
 print("\n")
-
-print("The deck contains: ", d)
-
+print("FOR TESTING PURPOSES - COMMENT OUT - The pond contains: ",pond)
 print("\n")
-print("Player one has: ", hands[0])
-
-
-#pile = d.remove(hands) # - hands
-
-#p = Pile(d.deal(1,52))
-#hands = p.deal(2,5)
-
-
-#print("The pile contains: ", pile)
-
-#print("The deck contains: ", d) 
-##print the player's hand only
-#print(hands[0])
-
-#print player 2's hand
-#print(hands[1])
-
-#print the pile
-#print(p)
+print("You have: ", p1hand)
+print("\n")
+print("Player 1 score: ", p1score)
+print("\n")
+print("Player 2 score: ", p2score)
 
 
 
+#guess = input("What card would you like to ask for?")
+#response = Card.get_from_typeable_name(guess)
 
+#print(response)
+
+# 'face', 'get_face_from_typeable_name', 'get_from_typeable_name', 'get_suit_from_typeable_name', 'get_typeable_name', 'suit'
+
+#for x in p1hand:
+ #   y= x.__str__()
+  #  if guess in y:
+   #     print(y)
+   # else:
+       # print("go fish!")
+
+#print(guess)
+
+
+#if guess in p1hand:
